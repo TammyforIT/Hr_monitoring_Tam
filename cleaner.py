2aff8832a8456ada15c675ce26cfbc71f2d8af16
Your Name <you@example.com>
2025-04-16 18:57:01 -0400

    t message-rm

--- cleaner.py
+++ cleaner.py
@@ -1,8 +1,15 @@
 def filter_nondigits(data: list) -> list:
+
     """
     INSERT DOCSTRING HERE
     """
     new_data = []
     for d in data:
-
+        if isinstance
+        new_data.append(d * 2)
+        data = [1,2,3,4,5]
+        new_data = [d * 2 for d in data] 
+        
+        print(new_data)
         return new_data
+
