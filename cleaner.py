b79b0227c6b92481b8a770a3d6e14a1978248d37
TammyforIT <tamtam113.tg@gmail.com>
2025-04-16 22:13:37 -0400

    Update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -1,13 +1,12 @@
 def filter_nondigits(data: list) -> list:
-    """
-    INSERT DOCSTRING HERE
-    """
-   def filter_nondigits(data: list) -> list:
+
     """
     INSERT DOCSTRING HERE
     """
     new_data = []
     for d in data:
+        if isinstance:
+            new_data.append(d * 2)
         data = [1,2,3,4,5]
         new_data = [d * 2 for d in data] 
         
