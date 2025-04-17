fbffe55f2e3e420285c8c7a4c3d947e50e575d39
Your Name <you@example.com>
2025-04-16 23:40:46 -0400

    update

--- metrics.py
+++ metrics.py
@@ -1,7 +1,7 @@
 def average(data: list) -> float:
     if not data:
-        raise ValueError
-    return average
+        raise ValueError("list cannot be empty")
+    return sum(data)/ len(data) #size data
     """
     Calculate average value of the list.
 
@@ -10,14 +10,14 @@ def average(data: list) -> float:
     Returns:
         float: a floating point value representing the average of this list
     """
-    average = 0
-    maximum = 0
+    #average = 0
+    #maximum = 0
 
 
 def maximum(data: list) -> float:
     if not data:
-        raise ValueError
-    return max
+        raise ValueError("list cannot be empty")
+    return max(data)
     """sn
     INSERT DOCSTRING HERE
     """
@@ -26,10 +26,10 @@ def maximum(data: list) -> float:
 
 def variance(data: list) -> float:
     if not data:
-        raise ValueError
-    n = len
-    mean = sum(data)
-    return sum
+        raise ValueError("list cannot be empty")
+    n = len(data)
+    mean = sum(data) /n
+    return sum((x-mean)**2 for x in data) /n
     """
     INSERT DOCSTRING HERE
     (calculate population variance)
@@ -39,8 +39,8 @@ def variance(data: list) -> float:
 
 def standard_deviation(data: list) -> float:
     if not data:
-        raise ValueError
-    return variance(data)
+        raise ValueError("cannot be empty")
+    return variance(data)**0.5 #if less than 1 its a 0
     """
     INSERT DOCSTRING HERE
     (calculate population standard deviation)
